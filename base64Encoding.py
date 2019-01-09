640a986119ccab155a9805e8a97939b9ba746fe9
Lynton Brown <Lyntball@hotmail.com>
2019-01-09 05:58:25 -0800

    Update base64Encoding.py

--- base64Encoding.py
+++ base64Encoding.py
@@ -1,3 +1,11 @@
+#! /bin/bash/env python3
+'''
+This is a quick function to use low level encoding. Base64 is not too
+complicated on its own; however coupled with Hashing creates a secure
+data structure.
+
+Created By Lynton Brown 01/09/2019
+'''
 import base64
 
 def Encode(data):
